c1484ecef8f86e168704a290c3f81141fb003077
muhammadulhussain <muhammad.65324@iqra.edu.pk>
2026-05-02 15:42:35 +0500

    Final cleanup: removed node_modules and fixed sync

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ye line zaroor add karein taake files sahi load hon
+  base: '/Hux_Ain-Portfolio/'
 })
